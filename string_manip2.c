91aadbc59f9009b4baedd228e9239b95cc81f75e
Imane47250 <elaceriimane@gmail.com>
2023-06-26 17:50:33 -0700

    manipulation strings

--- string_manip2.c
+++ string_manip2.c
@@ -67,3 +67,71 @@ char *custom_int_to_string(int num)
 	return (result);
 }
 
+/**
+ * custom_atoi - custom atoi function that converts a string to an int
+ * @str: string to be converted
+ * Owned by: Imane & Fatima Zahra
+ * Return: the converted number if successful
+ *      -1 if the string contains non-numbers
+ */
+int custom_atoi(char *str)
+{
+	int i = 0;
+	int num = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			num = num * 10 + (str[i] - '0');
+		}
+		else
+		{
+			return (-1);
+		}
+
+		i++;
+	}
+
+	return (num);
+}
+
+/**
+ * custom_ignore_first_c_strcat - concatenates two strings ignoring 1st char
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: concatenated string
+ */
+char *custom_ignore_first_c_strcat(char *dest, const char *src)
+{
+	int len = 0;
+	int len2 = 0;
+	int total_len = 0;
+	int j = 1;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+		total_len++;
+	}
+	while (src[len2] != '\0')
+	{
+		len2++;
+		total_len++;
+	}
+
+	dest = _realloc(dest, len, sizeof(char) * (total_len + 1));
+
+	if (dest == NULL)
+		return (NULL);
+
+	while (src[j] != '\0')
+	{
+		dest[len] = src[j];
+		len++;
+		j++;
+	}
+	dest[len] = '\0';
+
+	return (dest);
+}
