182b5b19944c8931acc18a75fd9aa99ac700cbe3
Imane47250 <elaceriimane@gmail.com>
2023-06-27 04:05:44 -0700

    modifying the exit file

--- exit.c
+++ exit.c
@@ -1,6 +1,5 @@
 #include "header.h"
 
-
 /**
  * exit_program - frees resources then exits
  * @exit_cmd: user's command for exit
@@ -10,24 +9,24 @@
  * Owned by: Imane & Fatima Zahra
  * Return: 0 if successful, 2 if failed
  */
+
 int exit_program(char **exit_cmd, list_t *env, int cmd_num, char **cmd)
 {
-	int exit_value = 0;
+	int e_value = 0;
 
 	if (exit_cmd[1] != NULL)
-		exit_value = custom_atoi(exit_cmd[1]);
+		e_value = custom_atoi(exit_cmd[1]);
 
-	if (exit_value == -1)
+	if (e_value == -1)
 	{
 		print_illegal_number_error(exit_cmd[1], cmd_num, env);
 		deallocate_strings(exit_cmd);
 		return (2);
 	}
-
 	deallocate_strings(exit_cmd);
 	free_linked_list(env);
-	deallocate_strings(cmd);
-
-	exit(exit_value);
+	if (cmd != NULL)
+		deallocate_strings(cmd);
+	exit(e_value);
 }
 
